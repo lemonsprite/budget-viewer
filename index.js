c6b7018fba60c5d3807a07ace517921ab40f1dc8
lemonsprite <alfath.noor17@gmail.com>
2022-03-28 14:48:44 +0700

    firebase integration + submiting data

---
+++ index.js
@@ -0,0 +1,23 @@
+// Initial Setup
+const form = document.querySelector('form')
+const nama = document.querySelector('#nama')
+const biaya = document.querySelector('#biaya')
+const error = document.querySelector('#error')
+
+// Submit Event
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    if(nama.value && biaya.value) {
+        let data = {
+            nama: nama.value,
+            biaya: parseInt(biaya.value)
+        }
+
+        db.collection('budgets').add(data).then(res => {
+            nama.value = biaya.value = error.text = "";
+        })
+    } else {
+        error.textContent = "Data tidak boleh kosong."
+    }
+})
